5d47b29d02ba81f6a57ae39b5e57e2829a6301d7
Anton Vershinin <a.vershinin92@gmail.com>
2023-08-11 14:24:49 +0300

    added calculator

---
+++ todo_app/src/calculator/Calculator.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import {View, Text} from 'react-native';
+
+type Props = {};
+
+const Calculator: React.FC<Props> = props => {
+  return <View></View>;
+};
+
+export default Calculator;
